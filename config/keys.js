c80dee545f5b4dad0249368aa2710bfb764e5454
lasithaprabodha <lasiprabo@gmail.com>
2017-10-18 17:42:13 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '858927437310-k1r2kcdg05egcvl2etuk7alkhprmnc8p.apps.googleusercontent.com',
+    googleClientSecret: 'UknO92Qv3RhebfJ78FOvIgdP'
+}
---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').googleStrategy;
+const keys = require('./config/keys');
+
+const app = express();
+
+passport.use(new googleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+    console.log(accessToken);
+}
+));
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
